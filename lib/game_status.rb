4911e1ab8cc6b830035cdbb63f938c4bb22065ab
Sarah Dunford <sarahbd97@gmail.com>
2020-08-25 20:47:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,7 @@ def won?(board)
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
-    elsif position_1 == "0" && position_2 == "0" && position_3 == "0"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     else
       false
