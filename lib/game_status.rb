3472854bfa48012b51f527cdebb6b62b26796617
Sarah Dunford <sarahbd97@gmail.com>
2020-08-25 21:33:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,8 +19,6 @@ def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     if win_combination.all? {|win_index| board[win_index] == "X"} || win_combination.all? {|win_index| board[win_index] == "O"}
       return win_combination
-    else
-      false
     end
   end
 end
