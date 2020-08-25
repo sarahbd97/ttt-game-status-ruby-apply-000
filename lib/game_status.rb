c94492081480b5728723418799f3b219df2417b3
Sarah Dunford <sarahbd97@gmail.com>
2020-08-25 21:11:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,10 +18,9 @@ WIN_COMBINATIONS = [
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
     win_combination.all? do |win_index|
-      position_taken?(board,win_index)
-      if board[win_index] == "X"
+      if position_taken?(board,win_index) && board[win_index] == "X"
         true
-      elsif board[win_index] == "O"
+      elsif position_taken?(board,win_index) && board[win_index] == "O"
         true
       else
         false
