48ce2c585767aff25e909983bca0618c838e3037
Sarah Dunford <sarahbd97@gmail.com>
2020-08-25 21:50:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,7 +39,10 @@ def draw?(board)
 end
 
 def over?(board)
-
+  if won?(board) == true || draw?(board) == true || full?(board) == true
+    return true
+  else
+    return false
 end
 
 def winner(board)
